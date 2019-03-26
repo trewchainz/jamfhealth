97b26d0190cd1057ec0adbb54f2184faa38647e8
trewchainz <relytetan@gmail.com>
2019-03-26 14:18:33 -0700

    create ze variables

--- jamf_heal.sh
+++ jamf_heal.sh
@@ -4,13 +4,20 @@
 #
 # Description: JAMF Mac client health check
 
+#ze variables
+date=`date`
+hostname=`hostname`
+manage=`echo "+++++++++++++ collecting jamf manage status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.logsudo jamf manage | sudo tee -a /library/logs/jamfhealth_\$hostname.log `
+mdm=`echo "+++++++++++++ collecting jamf mdm status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.log && sudo jamf mdm | sudo tee -a /library/logs/jamfhealth_\$hostname.log`
+recon=`echo "+++++++++++++ collecting jamf recon status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.log && sudo jamf recon | sudo tee -a /library/logs/jamfhealth_\$hostname.log`
+
+#check if root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-echo "+++++++++++++ collecting jamf manage status: `date` +++++++++++++\n" && sudo jamf manage | sudo tee -a /library/logs/jamfhealth_`hostname`.log 
- 
-echo "+++++++++++++ collecting jamf mdm status: `date` +++++++++++++\n" && sudo jamf mdm | sudo tee -a /library/logs/jamfhealth_`hostname`.log 
-
-echo "+++++++++++++ collecting jamf recon status: `date` +++++++++++++\n" && sudo jamf recon | sudo tee -a /library/logs/jamfhealth_`hostname`.log 
+#collect ze data
+echo $manage
+echo $mdm
+echo $recon
