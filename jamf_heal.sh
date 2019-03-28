9d48d2ba85cbba21b6e1fe678309312d637e00de
trewchainz <relytetan@gmail.com>
2019-03-28 11:30:10 -0700

    add reinstall logic

--- jamf_heal.sh
+++ jamf_heal.sh
@@ -7,7 +7,7 @@
 #ze variables
 date=`date`
 hostname=`hostname`
-manage=`echo "+++++++++++++ collecting jamf manage status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.logsudo jamf manage | sudo tee -a /library/logs/jamfhealth_\$hostname.log `
+manage=`echo "+++++++++++++ collecting jamf manage status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.log && sudo jamf manage | sudo tee -a /library/logs/jamfhealth_\$hostname.log `
 mdm=`echo "+++++++++++++ collecting jamf mdm status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.log && sudo jamf mdm | sudo tee -a /library/logs/jamfhealth_\$hostname.log`
 recon=`echo "+++++++++++++ collecting jamf recon status: \$date +++++++++++++\n" | sudo tee -a /library/logs/jamfhealth_\$hostname.log && sudo jamf recon | sudo tee -a /library/logs/jamfhealth_\$hostname.log`
 
@@ -21,3 +21,13 @@ fi
 echo $manage
 echo $mdm
 echo $recon
+echo "Would you like to re-install & re-enroll the client? (Have QuickAdd pkg in Downloads folder. Doesn't delete JSS record)..."
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) sudo jamf removeframework | sudo tee -a /library/logs/jamfhealth_\$hostname.log;
+			  read -p "Pause for removing JSS record [Press Enter]..."
+			  sudo installer -verbose -pkg ~/Downloads/QuickAdd_10_9.pkg -target / | sudo tee -a /library/logs/jamfhealth_\$hostname.log;;
+
+		No ) exit ;;
+	esac
+done
